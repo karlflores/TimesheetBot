185a9b307b2fa4d1b6a22377bd0a65f916eeccaf
karlflores <karl.flores.9@gmail.com>
2019-06-05 01:53:32 +1000

    making working on the database files

--- database_util.js
+++ database_util.js
@@ -1,28 +1,76 @@
+// This is only for development work
+require('dotenv').config()
+
 const mongo = require('mongodb')
 const mongoose = require('mongoose')
-//require('dotenv').config()
 
+// read in the password and username for the database from the env variables 
 const password = process.env.DB_PW 
 const username = process.env.DB_UN
-var uri = `mongodb+srv://${username}:${password}@timesheets-82gmj.mongodb.net/test?retryWrites=true&w=majority`
+
+// for username access. 
+var uri = `mongodb+srv://${username}:${password}@timesheets-82gmj.mongodb.net/test?retry\
+	Writes=true&w=majority`
+
+// Object storing the collections in our database 
 const COLLECTIONS = {
 	timesheets: "timesheets",
 	users: "users",
 }
 const DATABASE = "timesheet"
-const server = 'mongodb://localhost:27017/timesheet' 
-//uri = 'mongodb://localhost:27017/timesheet'
 
+// this is for local mongodb development 
+const server = 'mongodb://localhost:27017/timesheet' 
+
+// get all users stored in the database 
+getUsers = (callback) => {
+	mongo.connect(uri, (err,db) => {
+		var dbo = db.db(DATABASE)
+
+		// connect to the right collection and get all messages 
+	})
+}
+
+findAllFromCollection = (collection, query, callback) => {
+	_res = mongo.connect(uri, function(err, db) {
+		// error check first 
+		if (err) throw err;
+
+		// get the right database 
+		var dbo = db.db(DATABASE)
+		
+		// connect to the right collection and get an array of all messages 
+		dbo.collection(collection)
+					.find(query)
+					.toArray((err,res)=>{
+
+			if(err) throw err;	
+			// return the message using the callback function passed 
+			callback(res) 
+		})	
+
+		db.close();
+	});	
+	
+}
+
+// get all user messages from the database given a uri 
 getUserMessages = (msg,_uid,callback) => {
 	_res = mongo.connect(uri, function(err, db) {
-		var dbo = db.db(DATABASE)
+		// error check first 
 		if (err) throw err;
+
+		// get the right database 
+		var dbo = db.db(DATABASE)
+		
+		// connect to the right collection and get an array of all messages 
 		dbo.collection(COLLECTIONS.timesheets)
 					.find({_uid})
 					.toArray((err,res)=>{
 
 			if(err) throw err;
-			console.log(res)
+			
+			// return the message using the callback function passed 
 			callback(msg,res) 
 
 		})	
@@ -31,6 +79,7 @@ getUserMessages = (msg,_uid,callback) => {
 	});	
 }
 
+// function to delete a message using its message id
 deleteMessage = function(_id){
 	_res = mongo.connect(uri, function(err, db) {
 		var dbo = db.db(DATABASE)
@@ -43,7 +92,9 @@ deleteMessage = function(_id){
 	});	
 }
 
+// create and update happen in the same function 
 update = function(payload){
+
 	// lets parse the payload first 
 	query = {_id:payload._id};	
 	updated = {_uid:payload._uid,
@@ -51,8 +102,9 @@ update = function(payload){
 			code1:payload.code1,
 			code8:payload.code8,
 			_username:payload._username}	
-	console.log(query)		
 	
+	console.log(query)		
+		
 	mongo.connect(uri, function(err, db) {
 		var dbo = db.db(DATABASE)
 		if (err) throw err;
@@ -60,10 +112,13 @@ update = function(payload){
 		// check is there is an entry in the db already
 		dbo.collection(COLLECTIONS.timesheets).find(query).toArray((err,res)=>{
 			if(err) throw err;
-			dup = false;
+
 			dup = res.length > 0;
-			console.log(dup,res.length)
-			console.log(res)
+			console.log("Considering the following...")
+			console.log(res,'\n')
+			
+			// if there is already a message in the database we can just update 
+			// that message with the updated message.
 			if(dup === true){
 				// if there is an existing collection we just need to update the 
 				// values of this entry 
@@ -71,14 +126,14 @@ update = function(payload){
 							.updateOne(query,{$set:updated}, (err,res)=>{
 
 					if(err) throw err;	
+					
 					console.log(`Updating ${payload._username} entry`);
-
 				});
 
 			}else{					
 				// if there is nothing, we just need to add this message to the 
 				// timesheet 
-				console.log("ADDING ELEMENT")
+				console.log("ADDING ENTRY TO THE DATABASE...")
 				dbo.collection(COLLECTIONS.timesheets)
 							.insertOne(payload, (err,res)=>{
 
@@ -86,7 +141,6 @@ update = function(payload){
 					console.log(`Adding ${payload._username} entry`);
 
 				});
-
 			}
 			db.close();
 		});
